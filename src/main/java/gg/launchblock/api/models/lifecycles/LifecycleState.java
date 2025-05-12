a34a9dbf18a7fbaae5a1de8b9128c42d2d891505
Billy Robinson <hello@billy.ws>
2025-05-12 22:43:55 +0100

    [LB-55]: Add otel collector support for all backend APIs

--- src/main/java/gg/launchblock/api/models/lifecycles/LifecycleState.java
+++ src/main/java/gg/launchblock/api/models/lifecycles/LifecycleState.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum LifecycleState {
+    PREPARING(LifecycleStage.QUEUE),
     QUEUE(LifecycleStage.QUEUE),
     BUILDING(LifecycleStage.BUILD),
     BUILD_FAILED(LifecycleStage.FAILED),
